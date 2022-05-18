d65e65f4ac2ce595339371718a0a6c51058dc4de
Huimin Cao <huim.cao@gmail.com>
2022-05-19 00:12:06 +0200

    Add js

--- js/saved.js
+++ js/saved.js
@@ -1,5 +1,7 @@
 const bookmark = document.querySelectorAll('[data-js="bookmark"]');
 
 for (let i = 0; i < bookmark.length; i++) {
-  bookmark[i].classList.toggle("bookmark-saved");
+  bookmark[i].addEventListener("click", () => {
+    bookmark[i].classList.toggle("card-bookmark-saved");
+  });
 }
